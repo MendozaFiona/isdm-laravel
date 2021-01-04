9c30d0bc2a79be0dcc79665c5ce9125c6f231be0
unknown <lenalee.mendoza@gmail.com>
2021-01-05 03:31:21 +0800

    initial commit for front end

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return 'Hello World';
+Route::get('/login', function () {
+    return view('pages/login');
 });
+//Auth::routes();
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
